ea161f8415c2dc23db99ef8277c076fb0817e895
MauricioNC <mauricionc19@gmailc.om>
2022-12-08 15:45:23 -0600

    Modifying the raffle animation
    Was deleted the variables and properties to set the left, right, top and
    bottom values due to in the .scss files, a display flex is used, and
    this properties can be used with a display flex.
    
    The idea of the raffle method in the animations class is set the random
    time for the animation duration and the animation delay. The entire
    animation was created in the _exchange.scss file.

--- app/javascript/packs/animations.js
+++ app/javascript/packs/animations.js
@@ -4,17 +4,9 @@ export class Animations
 
   raffle(elem)
   {
-    let left = 10 + Math.random();
-    let right = 10 + Math.random();
-    let top = 10 + Math.random();
-    let bottom = 10 + Math.random();
     let time = 0.6 + Math.random();
 
     elem.style.animationDuraion = time + 's';
     elem.style.animationDelay = time + 's';
-    elem.style.left = left + 'px';
-    elem.style.right = right + 'px';
-    elem.style.top = top + 'px';
-    elem.style.bottom = bottom + 'px';
   }
 }
